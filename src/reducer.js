39c5bdcbe06fd4b094d1d660716e2319ea2440e3
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-01-13 13:23:41 -0800

    created the reducer

---
+++ src/reducer.js
@@ -0,0 +1,23 @@
+let lastId = 0;
+
+function reducer(state, action) {
+  // the job of an action is to return a new state based on the action.
+  if (action.type === 'bugAdded') {
+    // we will return a new state.
+    // we need to return a new state that is the copy of the old state with newly added data.
+    return [
+      ...state, 
+      {
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false
+      }
+    ]
+  } else if (action.type === 'bugRemoved') {
+    // here we want to return a new state with all the bugs except the one that we want to remove.
+    return state.filter(bug => bug.id !== action.payload.id)
+  }
+
+  // if the action does not match any of the actions we defined we want to return the current state. this will prevent our application from crashing
+  return state;
+}
